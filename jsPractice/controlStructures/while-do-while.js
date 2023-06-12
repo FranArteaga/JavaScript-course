40e31b70c1a975931a87bd10f9dab45e28ff93f3
francis <franrtg96@gmail.com>
2023-06-12 00:13:51 -0600

    made simple exercises with while and do-while

---
+++ jsPractice/controlStructures/while-do-while.js
@@ -0,0 +1,16 @@
+
+const cars = ['Ford', 'Mazda', 'Honda', 'Toyota']
+
+console.warn('-While-')
+let i = 0
+while (i < cars.length) {
+    console.log(cars[i])
+    i++
+}
+
+console.warn('-Do while-')
+let j = 0
+do {
+console.log(cars[j])
+ j++
+} while (cars[j])
