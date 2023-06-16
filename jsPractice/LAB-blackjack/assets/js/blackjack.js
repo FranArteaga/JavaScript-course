e4eb6c767335b23ec4e65be683b90bf7dc3453ed
francis <franrtg96@gmail.com>
2023-06-16 00:34:33 -0600

    created function that returns a card from the deck

--- jsPractice/LAB-blackjack/assets/js/blackjack.js
+++ jsPractice/LAB-blackjack/assets/js/blackjack.js
@@ -2,7 +2,7 @@ let deck = []
 let typesOfCard = ["C", "D", "H", "S"]
 let cardWithLetter = ["A", "J", "Q", "K"]
 
-
+//creating deck adding elements from array
 const createDeck = () => {
     for (let i = 2; i <= 10; i++) {
         for (let j = 0; j < typesOfCard.length; j++) {
@@ -10,11 +10,30 @@ const createDeck = () => {
         }
     }
 
-    for (let i = 0; i < cardWithLetter.length; i++){
-        for (let j = 0; j< typesOfCard.length; j++){
-            deck.push(cardWithLetter[i]+ typesOfCard[j])
+    for (let i = 0; i < cardWithLetter.length; i++) {
+        for (let j = 0; j < typesOfCard.length; j++) {
+            deck.push(cardWithLetter[i] + typesOfCard[j])
         }
     }
-    console.log(deck)
+    deck = _.shuffle(deck)
+    console.log("shuffled", deck)
+    return deck
 }
 createDeck()
+
+//returning card
+
+// let newCard = []
+const giveCard = () => {
+    if (deck.length === 0) {
+        throw 'No more cards!'
+    }
+    // newCard.push(deck.shift())
+    // console.log(newCard)
+    const card = deck.pop()
+    console.log(deck)
+    console.log(card)
+    return card
+
+}
+giveCard()
